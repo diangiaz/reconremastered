94e6e051d3d11207a87b4d70dcba826364d0a7ce
Gian Diaz <Gian Diaz>
2017-07-20 22:54:43 +0800

    Updated models.py

--- reconDjango/canvass/models.py
+++ reconDjango/canvass/models.py
@@ -1,159 +1,209 @@
 from django.db import models
 
 class User(models.Models):
-	ADMIN 		= 'Admin'
-	EMPLOYEE 	= 'Employee'
+	ADMIN = 'Admin'
+	EMPLOYEE = 'Employee'
 	EMPLOYEE_TYPE_CHOICES = (
 		(ADMIN, 'Admin'),
 		(EMPLOYEE, 'Employee'),
 	)
-	username 	= models.CharField(
-		max_length=25,
-		primary_key=true,
+	username = models.CharField(
+		max_length=25
 	)
-	password 	= models.CharField(
+	password = models.CharField(
+		max_length=25
+	)
+	name = models.CharField(
 		max_length=50
 	)
-	name		= models.CharField(
+	email = models.EmailField(
 		max_length=50
 	)
-	email		= models.EmailField(
-		max_length=254
-	)
-	branch		= models.CharField(
+	branch = models.CharField(
 		max_length=50
 	)
-	usertype	= models.CharField(
+	usertype = models.CharField(
 		max_length=10,
 		choices=EMPLOYEE_TYPE_CHOICES,
 	)
-	idnum		= models.IntegerField(
+	idnum = models.IntegerField(
+	)
+	userID = models.AutoField(
+		primary_key=true
 	)
 	
 class Device(models.Models):
-	ROUTER 		= 'Router'
-	SWITCH 		= 'Switch'
-	TERMINAL 	= 'Terminal'
-	SERVER 		= 'Server'
+	ROUTER = 'Router'
+	SWITCH = 'Switch'
+	TERMINAL = 'Terminal'
+	SERVER = 'Server'
 	DEVICE_TYPE_CHOICES = (
 		(ROUTER, 'Router'),
 		(SWITCH, 'Switch'),
 		(TERMINAL, 'Terminal'),
 		(SERVER, 'Server'),
 	)
-	type	= models.IntegerField(
+	type = models.IntegerField(
 		choices=DEVICE_TYPE_CHOICES
 	)
-	name	= models.CharField(
-		max_length=50
+	name = models.CharField(
+		max_length=25
 	)
-	ycord	= models.PositiveIntegerField(
+	ycord = models.PositiveIntegerField(
 	)
-	xcord	= models.PositiveIntegerField(
+	xcord = models.PositiveIntegerField(
+	)
+	deviceID = models.AutoField(
+		primary_key=true
 	)
 
 class Group(models.Models):
-	groupname	= CharField(
-		max_length=50,
-		primary_key=true,
+	groupname = models.CharField(
+		max_length=25
+	)
+	groupID = models.AutoField(
+		primary_key=true
 	)
 
 class Port(models.Models):
-	SERIAL 			= 'Serial'
-	GIGABYTE 		= 'Gigabyte'
-	FASTETHERNET	= 'Fast Ethernet'
-	CONSOLE			= 'Console'
+	SERIAL = 'Serial'
+	GIGABYTE = 'Gigabyte'
+	FASTETHERNET = 'Fast Ethernet'
+	CONSOLE = 'Console'
 	PORT_TYPE_CHOICES = (
 		(SERIAL, 'Serial'),
 		(GIGABYTE, 'Gigabyte'),
 		(FASTETHERNET, 'Fast Ethernet'),
 		(CONSOLE, 'Console'),
 	)
-	name		= models.CharField(
-		max_length=50
+	name = models.CharField(
+		max_length=25
 	)
-	number		= IntegerField(
+	portID = models.AutoField(
 		primary_key=true
 	)
-	type		= models.CharField(
+	type = models.CharField(
 		max_length=20,
 		choices=PORT_TYPE_CHOICES
 	)
-	deviceId	= models.CharField(
+	deviceID = models.IntegerField(
 		max_length=20
 	)
 
 class GroupToDevice(models.Models):
-	RESERVE 	= 'RS'
-	APPROVE 	= 'AP'
-	ALLOCATION 	= 'AL'
+	RESERVE = 'RS'
+	APPROVE = 'AP'
+	ALLOCATION = 'AL'
 	TYPE_CHOICES = (
 		(RESERVE, 'Reserve'),
 		(APPROVE, 'Approve'),
 		(ALLOCATION, 'Allocation'),
 	)
-	groupID		= CharField(
-		max_length=50
+	groupID = models.IntegerField(
 	)
-	deviceId	= CharField(
-		max_length=50
+	deviceId = models.IntegerField(
 	)
-	startDateTime	= DateTimeField(
+	startDateTime = models.DateTimeField(
 		auto_now=False,
 		auto_now_add=False,
 		)
-	endDateTime		= DateTimeField(
+	endDateTime = models.DateTimeField(
 		auto_now=False,
 		auto_now_add=False,
 	)
-	type	= models.CharField(
+	type = models.CharField(
 		max_length = 2,
 		choices = TYPE_CHOICES,
 	)
 	
 class UserToGroup(models.Models):
-	userID	= CharField(
-		max_length=24
+	userID = models.IntegerField(
 	)
-	groupID	= CharField(
-		max_length=50
+	groupID	= models.IntegerField(
 	)
 
 class Connections(models.Models):
-	groupID		= CharField(
-		max_length=50
+	CONSOLE = 'Console'
+	SERIAL = 'Serial'
+	STRAIGHT = 'Straight'
+	CONNECTION_TYPE_CHOICES = (
+		(CONSOLE, 'Console'),
+		(SERIAL, 'Serial'),
+		(STRAIGHT, 'Straight'),
 	)
-	srcdevID	= CharField(
-		max_length=50
+	groupID = models.IntegerField(
 	)
-	srcdevPort	= 
-	destdevID
-	destdevPort
-	cabletype
+	srcdevID = models.IntegerField(
+	)
+	srcdevPort = models.IntegerField(
+	)
+	destdevID = models.IntegerField(
+	)
+	destdevPort	= models.IntegerField(
+	)
+	cabletype = models.CharField(
+		max_length=10,
+		choices=CONNECTION_TYPE_CHOICES,
+	)
 
 class Config(models.Models):
-	groupID
-	devID
-	config
+	groupID = models.IntegerField(
+	)
+	devID = models.IntegerField(
+	)
+	config = models.FileField(
+		upload_to=None,
+		max_length=100,
+	)
+	configID = models.AutoField(
+		primary_key=true
+	)
 
 class SaveTopology(models.Models):
-	groupID
-	name
+	groupID = models.IntegerField(
+	)
+	name = models.CharField(
+		max_length = 40
+	)
+	saveID = models.AutoField(
+		primary_key=true
+	)
 
 class SaveConn(models.Models):
-	saveID
-	srcdevID
-	srcdevPort
-	destdevID
-	destdevPort
-	cabletype
+	CONSOLE = 'Console'
+	SERIAL = 'Serial'
+	STRAIGHT = 'Straight'
+	CONNECTION_TYPE_CHOICES = (
+		(CONSOLE, 'Console'),
+		(SERIAL, 'Serial'),
+		(STRAIGHT, 'Straight'),
+	)
+	saveID = models.AutoField(
+		primary_key=true
+	)
+	srcdevID = models.IntegerField(
+	)
+	srcdevPort = models.IntegerField(
+	)
+	destdevID = models.IntegerField(
+	)
+	destdevPort = models.IntegerField(
+	)
+	cabletype = models.CharField(
+		max_length=10,
+		choices=CONNECTION_TYPE_CHOICES,
+	)
 
 class SaveDev(models.Models):
-	saveID
-	type
-	name
-	xcord
-	ycord
+	saveID = models.AutoField(
+		primary_key=true
+	)
+	xcord = models.IntegerField(
+	)
+	ycord = models.IntegerField(
+	)
+	devID = models.IntegerField(
+	)
 
 
 # Create your models here.
