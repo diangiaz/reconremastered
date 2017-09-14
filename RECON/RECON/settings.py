1c9a7a9e60fe8004e968daa8d359201107015cb3
ariseus <renzellbarros@yahoo.com>
2017-09-14 10:45:26 +0800

    updaterenzell

--- RECON/RECON/settings.py
+++ RECON/RECON/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-	os.path.join('Activity', '..', 'External'),
+	os.path.join('Coordinator', '..', 'External'),
 )
 
 STATICFILES_FINDERS = (
